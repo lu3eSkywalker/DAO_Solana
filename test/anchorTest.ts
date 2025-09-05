18b1f7e83f01235b33a788323ee624480f25928e
lu3eskywalker <surajjhajhria1@gmail.com>
2025-09-05 23:52:55 +0530

    adding a creating proposal test case

--- test/anchorTest.ts
+++ test/anchorTest.ts
@@ -14,11 +14,10 @@ describe("Test", () => {
   // Generate a new keypair for the DAO account
   const daoAccountKeypair = web3.Keypair.generate();
 
-  // Generate a new transasction keypair for the multisig account
-  const transactionKeypair = web3.Keypair.generate();
+  // Generate a new proposal keypair for the proposal account
+  const proposalKeypair = web3.Keypair.generate();
 
   it("creates a multisig", async () => {
-
     const user1PublicKey = new web3.PublicKey("HVw1Z2KFYfKjdL2UThi5RGBvSUpsF4zdsPrucV8TggQm");
     const user2PublicKey = new web3.PublicKey("7eacdg5tZYPPqNdhi9PHvP5TUCEt9RjgUyoJL1a6L8JA");
     const user3PublicKey = new web3.PublicKey("8tbeZfMaQRfqYVCeaL5gnjn7nGMeKezYNe7c6tLwAK5X");
@@ -63,4 +62,39 @@ describe("Test", () => {
     const daoAccount = await program.provider.connection.getAccountInfo(daoAccountKeypair.publicKey);
     console.log("Multisig account created with data: ", daoAccount);
   });
+
+  it("create a dao proposal", async() => {
+
+    const daoPubkey = daoAccountKeypair.publicKey;
+    const title = "Token Minting";
+    const description = "Should we mint more tokens?";
+    
+    const options = [
+      { text: "Yes", vote_count: new anchor.BN(0) },
+      { text: "No", vote_count: new anchor.BN(0) }
+    ];
+
+   const instructionData = Buffer.from([
+      0x3b, 0x84, 0x18, 0xf6, 0x7a, 0x27, 0x08, 0xf3,
+      0x00, 0xf2, 0x05, 0x2a, 0x01, 0x00, 0x00, 0x00
+    ]);
+
+    const programId = new web3.PublicKey("9xBdHWanyjR6U84K7p59A2757bXH7Zhjvh3NdbkMbuwb");
+
+    const txHash = await program.methods
+    .createProposal(daoPubkey, title, description, programId, instructionData, options)
+    .accounts({   
+      proposal: proposalKeypair.publicKey,
+      proposer: program.provider.publicKey,
+      daoinfo: daoAccountKeypair.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    })
+    .signers([proposalKeypair])
+    .rpc()
+
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+
+    // Confirm Transaction
+    await program.provider.connection.confirmTransaction(txHash);
+  })
 });
